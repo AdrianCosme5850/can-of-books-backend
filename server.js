bb364d01c6786a6886a4de9693881af632cdeed0
Adrian Cosme-Halverson <adrian.cosme5850@gmail.com>
2022-08-29 17:22:41 -0700

    Added mongoose and database functionality.

--- server.js
+++ server.js
@@ -3,16 +3,34 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const Books = require('./book.js')
+const mongoose = require('mongoose')
+
 
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('mongoose is connected')
+})
+
+mongoose.connect(process.env.DB_URL);
+
+const PORT = process.env.PORT || 3002;
 
 app.get('/test', (request, response) => {
 
   response.send('test request received')
 
 })
+app.get('/books', getBooks);
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+async function getBooks(req, res){
+
+  let results = await Books.find();
+  res.send(results)
+}
