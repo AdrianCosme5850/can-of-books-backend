5d6c0368e8b10ba78bfe3789dfe9f28b1525fc94
Adrian Cosme-Halverson <adrian.cosme5850@gmail.com>
2022-08-30 19:35:11 -0700

    Added POST and DELETE paths for our booklist

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const Books = require('./book.js')
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { findByIdAndDelete } = require('./book.js');
 
 
 const app = express();
 app.use(cors());
-
+app.use(express.json());
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
@@ -26,6 +27,8 @@ app.get('/test', (request, response) => {
 
 })
 app.get('/books', getBooks);
+app.post('/books', createBooks);
+app.delete('/books/:id', deleteBooks);
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
@@ -33,4 +36,16 @@ async function getBooks(req, res){
 
   let results = await Books.find();
   res.send(results)
+}
+async function createBooks(req, res){
+  let createdBooks = await Books.create(req.body)
+  res.send(createdBooks);
+}
+async function deleteBooks(req, res, next){
+  try{
+    await Books.findByIdAndDelete(req.params.id)
+  }
+  catch(error){
+    next(error)
+  }
 }
